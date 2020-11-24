0b1d13c6200633d570ad10d267187e1399604164
Haltarys <gregoire.haardt@gmail.com>
2020-11-24 15:07:15 +0100

    Fix assert in Ackermann function and add main function

--- ackermann.c
+++ ackermann.c
@@ -15,11 +15,24 @@
 int ackermann(int m, int n)
 {
     // error checking
-    assert(m < 0 || n < 0);
+    assert(m >= 0 && n >= 0);
 
     if (m == 0)
         return n + 1;
     if (n == 0)
         return ackermann(m - 1, 1);
     return ackermann(m - 1, ackermann(m, n - 1));
+}
+
+int main(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf("ackermann(%i, %i) = %i\n", i, j, ackermann(i, j));
+        }
+    }
+
+    return 0;
 }
