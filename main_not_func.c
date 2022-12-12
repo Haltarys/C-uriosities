ae99e79f646bf068df75e15e96a48f5d9d4827d3
Haltarys <45515869+Haltarys@users.noreply.github.com>
2022-12-12 22:38:54 +0100

    Add snippet: main is not a function

---
+++ main_not_func.c
@@ -0,0 +1,9 @@
+// This is the smallest C program that can be compiled.
+// gcc compiles it (although with warnings) but g++ does not.
+// When omitted, the type of functions and global variables
+// is int by default and main isn't required to be a function in C.
+// It does need to be defined though.
+// Of course the program segfaults when executed because it
+// calls the function main, which in this case isn't a function.
+
+main;
